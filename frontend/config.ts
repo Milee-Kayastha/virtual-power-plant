6849ba4e8dd4b90cf386e658032b710760ffe3d1
Milee_Kayastha <mkayastha@hamropatro.com>
2024-02-26 22:21:37 +0545

    minor changes to config variables

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,1 +1,2 @@
-export let backend_url = "http://localhost:4000/"
+export let backend_url = process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:4000/"
+// export let backend_url = "https://virtual-power-plant-backend.vercel.app/"
